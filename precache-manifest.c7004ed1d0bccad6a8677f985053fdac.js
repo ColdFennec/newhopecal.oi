7def7f47afc5700a6db168363b11861359ae0493
ColdFennec <xColdLight@Outlook.com>
2021-02-17 13:43:44 +0100

    Update version

---
+++ precache-manifest.c7004ed1d0bccad6a8677f985053fdac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ec03f6eddced5a816b",
+    "url": "/newhopecal/css/app.d0cbf664.css"
+  },
+  {
+    "revision": "646088646093ad681e66cb57eb2c3c46",
+    "url": "/newhopecal/img/clock.64608864.svg"
+  },
+  {
+    "revision": "3deaebea4c547b52859ffe907984f1d1",
+    "url": "/newhopecal/index.html"
+  },
+  {
+    "revision": "95ec03f6eddced5a816b",
+    "url": "/newhopecal/js/app.fa7ceb06.js"
+  },
+  {
+    "revision": "7068f23af0c044039754",
+    "url": "/newhopecal/js/chunk-vendors.bdae873b.js"
+  },
+  {
+    "revision": "cf8713d972bc1651f3357552e204134a",
+    "url": "/newhopecal/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/newhopecal/robots.txt"
+  }
+]);
